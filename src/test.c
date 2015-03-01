3c0c05004c8cd7cd38f1ccf33fe2f8eb3cae119a
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-01 13:13:03 +0100

    update test.c

--- src/test.c
+++ src/test.c
@@ -1,6 +1,7 @@
 #include "am2315.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char **argv){
 	char *i2c_device = "/dev/i2c-1";
@@ -11,8 +12,14 @@ int main(int argc, char **argv){
 	
 	if(am != NULL){
 		float temperature, humidity;
-		am2315_read_data(am, &temperature, &humidity);
-		printf("temperature = %f, humidity = %f\n", temperature, humidity);
+		int i;
+		for(i = 0; i < 10; i++) {
+			printf("-------- %i ---------\n", i);
+			usleep(2*1000*1000);
+			am2315_read_data(am, &temperature, &humidity);
+			printf("temperature = %f, humidity = %f\n", temperature, humidity);
+		
+		}
 		am2315_close(am);
 	}
 	
